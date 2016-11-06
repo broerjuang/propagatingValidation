379663e42190de431a68a88f9c898ac1aee734c1
broerjuang <juangwiantoro@gmail.com>
2016-11-06 17:08:31 +0700

    CRUD controllers are done

--- controllers/event.server.controller.js
+++ controllers/event.server.controller.js
@@ -18,5 +18,39 @@ module.exports = {
     Event.find()
       .then(result => res.json(result))
       .catch(err => res.json(err))
+  },
+
+  findById: (req, res) => {
+    Event.findOne({_id: req.params.id})
+      .then(event => res.json(event))
+      .catch(err => res.json(err))
+  },
+
+  update: (req, res) => {
+    var id = {
+      _id : req.params.id
+    }
+
+    var updateEvent = {
+      name : req.body.name,
+      date : req.body.date,
+      description : req.body.description,
+      email : req.body.email
+    }
+
+    var option = {
+      new : true,
+      upsert  : true
+    }
+    Event.findOneAndUpdate(id, updateEvent)
+      .then(updated => res.json(updated))
+      .catch(err => res.json(err))
+  },
+
+  delete: (req, res) => {
+    Event.findOneAndRemove({_id: req.params.id})
+      .then(() => Event.find()))
+      .then(result => res.json(result))
+      .catch(err => res.json(err))
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET All Event. */
 router.get('/event', EventController.list);
+
+/* POST Event */
 router.post('/event', EventController.create)
 
+/* GET Event By Id */
+router.get('/event/:id', EventController.findById)
+
+/* PUT event By its id */
+router.put('/event/:id', EventController.update)
+
+/* Delete event By its id */
+router.put('/event/:id', EventController.update)
 module.exports = router;
